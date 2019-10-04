68aa1aa70e1aaef81a2f9564e984d0fef832afe1
matt-winzer <matt.winzer@gmail.com>
2019-10-04 14:55:14 -0600

    Add error handling

--- server.js
+++ server.js
@@ -8,4 +8,18 @@ app.get('/error', (req, res, next) => {
   res.json({ msg: 'hit me baby' })
 })
 
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(request, response, next) {
+  response.status(403).send('403: access denied.');
+  return;
+}
+
+function errorHandler(err, request, response, next) {
+  console.error('ERROR', err.message);
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined;
+  response.status(500).send({ error: err.message, stack, url: request.originalUrl });
+}
+
 app.listen(port, () => console.log(`Server running on port: ${port}`))
