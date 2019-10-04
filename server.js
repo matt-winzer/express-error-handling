0b7dbbdd093ef7ac3a20f435c975e5bb8eb193e9
matt-winzer <matt.winzer@gmail.com>
2019-10-04 14:54:28 -0600

    Add basic GET route

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use(express.json())
+
+app.get('/error', (req, res, next) => {
+  res.json({ msg: 'hit me baby' })
+})
+
 app.listen(port, () => console.log(`Server running on port: ${port}`))
