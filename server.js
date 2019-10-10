be4a80c5088af32863e205459738549c2584d4da
matt-winzer <matt.winzer@gmail.com>
2019-10-10 10:07:30 -0600

    Test error handling complete

--- server.js
+++ server.js
@@ -4,22 +4,35 @@ const port = process.env.PORT || 3000
 
 // app.use(express.json())
 
-app.get('/error', (req, res, next) => {
-  res.json({ msg: 'hit me baby' })
+app.get('/error', async (req, res, next) => {
+  const promise = new Promise((resolve, reject)=> {
+    setTimeout(function () {
+      const error = new Error('aw shucks')
+      reject(error)
+    }, 500)
+  })
+
+  try {
+    const result = await promise
+    res.json({ msg: 'hit me baby', result: result })
+  } catch (error) {
+    error.message = 'something else'
+    next(error)
+  }
+  
 })
 
 app.use(notFound)
 app.use(errorHandler)
 
 function notFound(request, response, next) {
-  response.status(403).send('403: access denied.');
-  return;
+  response.status(403).send('403: access denied.')
 }
 
 function errorHandler(err, request, response, next) {
-  console.error('ERROR', err.message);
-  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined;
-  response.status(500).send({ error: err.message, stack, url: request.originalUrl });
+  console.error('ERROR', err.message)
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  response.status(500).send({ error: err.message, stack, url: request.originalUrl })
 }
 
 app.listen(port, () => console.log(`Server running on port: ${port}`))
